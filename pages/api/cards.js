6cae4215dafabfacb0e6fe9ed64a73cd54cd2d97
Lee Ingram <lee@soylentcola.com>
2021-11-01 23:12:25 -0700

    Updates

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { getAllCardIds, getCardData } from '../../lib/cards'
+import { getAllCardIds, getCardData, getSortedCardsData } from '../../lib/cards'
 
 export default async (req, res) => {
-  const cards = await getAllCardIds();
+  const cards = await getSortedCardsData();
+  /*
   let cardArray = [];
   for(let i=0;i<cards.length;i++) {
     console.log(cards[i])
     cardArray.push(await getCardData(cards[i].params.id))
   }
-  res.status(200).json(cardArray)
+  */
+  res.status(200).json(cards)
 }
